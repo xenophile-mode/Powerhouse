6fafc09004f3c78a8c9dde09386edad3ea2ab445
xenophile-mode <erickgpersonalacct1@gmail.com>
2021-07-05 12:48:01 -0500

    updaed new and improved menu functions

--- Powershelltest.ps1
+++ Powershelltest.ps1
@@ -1,15 +1,41 @@
+#Script/command functions
 Function GetVolume {Get-Printer ; Get-Disk | Out-Default}
 
+#Menu Title Functions
+Function MenuTitle {
+     Write-Host ""	 
+	 Write-Host "______                      _ "
+	 Write-Host "| ___ \                    | |"      
+     Write-Host "| |_/ /____      _____ _ __| |__   ___  _   _ ___  ___ "      
+     Write-Host "|  __/ _ \ \ /\ / / _ \ '__| '_ \ / _ \| | | / __|/ _ \"       
+     Write-Host "| | | (_) \ V  V /  __/ |  | | | | (_) | |_| \__ \  __/"        
+     Write-Host "\_|  \___/ \_/\_/ \___|_|  |_| |_|\___/ \__,_|___/\___|  V0.1"    
+     Write-Host ""
+	 Write-Host " -Author: Erick Gonzales"
+     Write-Host " -Tools: 4"	 
+     Write-Host ""
+	 Write-Host ""
+     Write-Host ""
+	 
+     Write-Host "====================== $Title ======================"
+     Write-Host ""
+}
 
+
+#Test submenu
 function OtherMenu
 {
-     
-	 Write-Host 'Other Menu:
-
-  1. Get Volume
-  2. Test
-  3. Main Menu
-  q. Quit'
+     param (
+           [string]$Title = 'Other Menu'
+     )
+	 MenuTitle
+	 Write-Host '
+  [ 1 ] Get Volume
+  [ 2 ] Test
+  [ 3 ] Main Menu
+  
+  [ q ] Quit
+  '
 
 
 While (($IDSelection = Read-Host -Prompt 'Please select an option') -notin 1,2,3,'q') 
@@ -18,9 +44,11 @@ While (($IDSelection = Read-Host -Prompt 'Please select an option') -notin 1,2,3
 }
 
 Switch ($IDSelection) {
-    1 { GetVolume }
-    2 { echo test }
+    1 { cls ; GetVolume }
+    2 { cls ; echo test }
 	3 { cls ; Show-MainMenu }
+	
+	'q' { exit }
 }
 	 OtherMenu
 }
@@ -28,12 +56,16 @@ Switch ($IDSelection) {
 
 function Show-MainMenu
 {
-     
-	 Write-Host 'Main Menu:
-
-  1. GetVolume
-  2. Other Menu
-  q. Quit'
+     param (
+           [string]$Title = 'Main Menu'
+     )
+	 MenuTitle
+	 Write-Host '
+  [ 1 ] GetVolume
+  [ 2 ] Other Menu
+  
+  [ q ] Quit
+  '
 
 
 While (($IDSelection = Read-Host -Prompt 'Please select an option') -notin 1,2,'q') 
@@ -44,6 +76,7 @@ While (($IDSelection = Read-Host -Prompt 'Please select an option') -notin 1,2,'
 Switch ($IDSelection) {
     1 { GetVolume }
     2 { cls ; OtherMenu }
+    'q' { exit }
 }
 	 Show-MainMenu
 }
