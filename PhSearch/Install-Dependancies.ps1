29a12b96981d997495f523dc8b1949df0d5d9379
xenophile-mode <erickgpersonalacct1@gmail.com>
2021-08-12 09:14:11 -0500

    cleaning up install dependancies script

--- PhSearch/Install-Dependancies.ps1
+++ PhSearch/Install-Dependancies.ps1
@@ -1,7 +1,15 @@
-Function InstallPM {Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) ; Write-Host "Installing Scoop" ; Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')}
+Function InstallPM {
+	Set-ExecutionPolicy Bypass -Scope Process -Force; `
+	[System.Net.ServicePointManager]::SecurityProtocol = `
+	[System.Net.ServicePointManager]::SecurityProtocol -bor 3072; ` 
+	iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))  
+	; Write-Host "Installing Scoop" ; Invoke-Expression `
+	(New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+}
 
-Function InstallMP {  Write-Host "Installing ntop"; scoop install ntop; Write-Host "Installing Speedtest-CLI" ; scoop insatll speedtest-cli; Write-Host "installing fzf" ; scoop insall fzf ; Write-Host "Installing PSWindowsUpdate Module" ; Set-ExecutionPolicy RemoteSigned ; Install-Module PSWindowsUpdate ; Import-Module PSWindowsUpdate ; Install-Module -Name wifiprofilemanagement ; Import-Module -Name wifiprofilemanagement | Out-Default }
-
+Function InstallMP {
+	Write-Host "Installing ntop"; scoop install ntop; Write-Host "Installing Speedtest-CLI" ; scoop insatll speedtest-cli; Write-Host "installing fzf" ; scoop insall fzf ; Write-Host "Installing PSWindowsUpdate Module" ; Set-ExecutionPolicy RemoteSigned ; Install-Module PSWindowsUpdate ; Import-Module PSWindowsUpdate ; Install-Module -Name wifiprofilemanagement ; Import-Module -Name wifiprofilemanagement | Out-Default 
+}
 
 function DependancyInstaller
 {
